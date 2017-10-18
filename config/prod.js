2a3dc533868e212973a1f2d3ac035f2bdc26d7c2
christianhoward <christian.john.howard@gmail.com>
2017-10-18 14:20:25 -0400

    Added cookie key to prod keys

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    COOKIE_KEY: process.env.COOKIE_KEY
 };
