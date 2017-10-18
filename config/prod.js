57dc86f8d3a74641c66ea76d6bf6d9e4e0dd99b3
christianhoward <christian.john.howard@gmail.com>
2017-10-18 16:31:40 -0400

    Fixed cookie key issue

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    COOKIE_KEY: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY
 };
