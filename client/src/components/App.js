1a246057d58b9d9468e5b98a4229a90cdabcef2b
christianhoward <christian.john.howard@gmail.com>
2017-10-18 16:08:03 -0400

    Altered client to test OAuth in Production

---
+++ client/src/components/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+// import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+// import * as actions from '../actions';
+
+// import Header from './Header';
+// import Landing from './Landing';
+// import Dashboard from './Dashboard';
+// import SurveyNew from './surveys/SurveyNew';
+
+class App extends Component {
+    // componentDidMount() {
+    //     this.props.fetchUser();
+    //     console.log(this.props);
+    // }
+    // render() {
+    //     return (
+    //         <div className="container">
+    //             <BrowserRouter>
+    //                 <div className="container">
+    //                     <Header />
+    //                     <Route exact path="/" component={Landing} />
+    //                     <Route exact path="/surveys" component={Dashboard} />
+    //                     <Route path="/surveys/new" component={SurveyNew} />
+    //                 </div>
+    //             </BrowserRouter>
+    //         </div>
+    //     );
+    // }
+    render() {
+        return(
+        <div><a href="/auth/google">Login with Google</a></div>
+        );
+    }
+};
+
+//not using mapStateToProps, so null is the first value for the connect function
+// export default connect(null, actions)(App);
+export default App;
